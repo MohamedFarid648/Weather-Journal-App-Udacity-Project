1b216640dbd98640656afea09d396b9cfeda101c
MohamedFarid648 <mohamedfarid648@gmail.com>
2020-10-14 09:50:13 +0200

    feat: Meat the suggestions

--- website/app.js
+++ website/app.js
@@ -3,7 +3,7 @@
 
 
 // Personal API Key for OpenWeatherMap API
-const apiKey = '&APPID=3794968e819429b52119daf3aeb37b53';
+const apiKey = '&APPID=3794968e819429b52119daf3aeb37b53&units=imperial';
 let countryCode = '';//Cairo,eg
 const baseURL = `http://api.openweathermap.org/data/2.5/weather?q=`
 
